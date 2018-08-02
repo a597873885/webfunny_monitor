a20c066a232b7a4b677ab2e40aa3140aa8864154
jeffery <jiang1125712@163.com>
2018-08-02 17:11:13 +0800

    如果上传日志失败，依然清空本地数据，以防止不停的存储数据

--- webmonitor.js
+++ webmonitor.js
@@ -37,7 +37,7 @@
     , WEB_MONITOR_IP = 'www.webfunny.cn'
 
     // 上传数据的uri, 区分了本地和生产环境
-    , HTTP_UPLOAD_URI =  WEB_LOCATION.indexOf(WEB_LOCAL_IP) == -1 ? WEB_HTTP_TYPE + WEB_MONITOR_IP : WEB_HTTP_TYPE + WEB_LOCAL_IP + ':3000'
+    , HTTP_UPLOAD_URI =  WEB_LOCATION.indexOf(WEB_LOCAL_IP) == -1 ? WEB_HTTP_TYPE + WEB_MONITOR_IP : WEB_HTTP_TYPE + WEB_LOCAL_IP + ':8010'
 
     // 上传数据时忽略的uri, 需要过滤掉监控平台上传接口
     , WEB_MONITOR_IGNORE_URL = HTTP_UPLOAD_URI + '/api/v1/uploadLog'
@@ -223,6 +223,10 @@
               localStorage[JS_ERROR] = "";
               localStorage[CUSTOMER_PV] = "";
             }
+          }, function () {
+            localStorage[ELE_BEHAVIOR] = "";
+            localStorage[JS_ERROR] = "";
+            localStorage[CUSTOMER_PV] = "";
           })
         }
         timeCount = 0;
