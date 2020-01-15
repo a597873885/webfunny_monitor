ed2958fcd9a3e0e9cc1dd961bbd417bb0fa5a954
jeffery <jiang1125712@163.com>
2020-01-15 21:33:30 +0800

    修复接口耗时的bug

--- config/consts.js
+++ config/consts.js
@@ -4,7 +4,12 @@ const UPLOAD_TYPE = {
     RESOURCE_ERROR: "resource_error",
     HTTP_ERROR: "http_error",
     NEW_CUSTOMER: "new_customer",
-    INSTALL_COUNT: "install_count"
+    INSTALL_COUNT: "install_count",
+    HTTP_COUNT_A: "http_count_a",  // 接口耗时<=2s的
+    HTTP_COUNT_B: "http_count_b",  // 接口耗时2s< <=5s的
+    HTTP_COUNT_C: "http_count_c",  // 接口耗时5s< <=10s的
+    HTTP_COUNT_D: "http_count_d",  // 接口耗时10s< <=30s的
+    HTTP_COUNT_E: "http_count_e",  // 接口耗时>=30s的
 }
 const PROJECT_INFO = {
     PROJECT_VERSION: "",
