439bcce18902cfa83b7388126afd495ff0216b4f
jeffery <jiang1125712@163.com>
2020-03-02 15:54:27 +0800

    调整配置的提示

--- config/createConfig.js
+++ config/createConfig.js
@@ -4,8 +4,8 @@ module.exports = {
         localServerPort: '8011',  // 日志服务端口号
         localAssetsPort: '8010',  // 可视化系统端口号
 
-        messageQueue: false,  // 是否开启消息对列
+        messageQueue: false,  // 是否开启消息对列，订阅版可以开启
         purchaseCode: 'AAAABBBBCCCCDDDD', // 激活码
-        email: '',  // 接收警报的邮箱
+        email: '',  // 接收警报的邮箱（只支持163邮箱）
         emailPassword: '' // 邮箱密码
 }
