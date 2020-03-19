bcb0a309d933c9955af65ac267b4eaaceaccfe74
jeffery <jiang1125712@163.com>
2020-03-19 23:05:32 +0800

    调整激活码

--- config/createConfig.js
+++ config/createConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    purchaseCode: 'KDWHMQ2ZDNK0K73ZIUTZ',      // 填写激活码
+    purchaseCode: 'HSVHN2HDDOKFK8ZZEKQZ',      // 填写激活码
 
     webfunnyNeedLogin: false,              // 前端是否验证登录状态
                                            // 开启登录验证后，只能看到自己创建的项目。
