e31049f8969d6f9b001891c4c8d716daa94265ab
jeffery <jiang1125712@163.com>
2020-03-23 15:16:30 +0800

    调整创建规则

--- config/db_local.js
+++ config/db_local.js
@@ -2,10 +2,10 @@
  * 你可以在这个文件里配置你自己的数据库
  */
 const defaultConfig = {
-  ip: "",                 // mysql数据库所在云服务器的ip地址
-  dataBaseName: "",       // 数据库名称（如：webfunny_db）
-  userName: "",           // mysql的登录名
-  password: ""            // mysql的登录密码
+  ip: "180.76.183.218",                 // mysql数据库所在云服务器的ip地址
+  dataBaseName: "webfunny_db",       // 数据库名称（如：webfunny_db）
+  userName: "root",           // mysql的登录名
+  password: "webfunny_db_customer"            // mysql的登录密码
 }
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(defaultConfig.dataBaseName, defaultConfig.userName, defaultConfig.password, {
