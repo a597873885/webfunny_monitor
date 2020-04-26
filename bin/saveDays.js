60d15a824fe692c82442471e004d17ab587c0fe8
jeffery <jiang1125712@163.com>
2020-04-27 00:13:10 +0800

    调整存储周期

---
+++ bin/saveDays.js
@@ -0,0 +1,3 @@
+module.exports = {
+    days: 8
+  }
--- config/AccountConfig.js
+++ config/AccountConfig.js
@@ -1,4 +1,5 @@
 const CreateConfig = require("./createConfig")
+const { days } = require("../bin/saveDays")
 /**
  * 这是用户的私人配置文件。
  * @param purchaseCode 注册码。
@@ -17,6 +18,8 @@ const accountInfo = {
     ///////////////////////////////
 
     targetEmail: CreateConfig.email, // 目标邮箱（接收警报的邮箱）
+
+    saveDays: days, // 日志保存周期
 
     showPurchaseList: 1
 }
