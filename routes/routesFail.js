ca0fecd980a2a0cbdc26164a0400174e5ab4418c
jiangyw <jiangyw@fenqi.im>
2020-11-18 21:14:53 +0800

    增加必须要选择所属团队的验证条件

--- routes/routesFail.js
+++ routes/routesFail.js
@@ -1,10 +1,19 @@
 const Fail = require('../controllers/fail')
-const { Common } = require("../controllers/controllers.js")
+const { Common, UserController } = require("../controllers/controllers.js")
 const createRoutesFail = (router) => {
     // 用户激活码无效
     router.get('/getSysInfo', Common.getSysInfo);
 
     router.post('/createPurchaseCode', Fail.createPurchaseCode);
+
+    // 管理员注册接口
+    router.post('/registerForAdmin', UserController.registerForAdmin);
+    
+    // 注册用户
+    router.get('/register', UserController.register);
+
+    // 登录
+    router.post('/login', UserController.login);
 }
 
 module.exports = {
