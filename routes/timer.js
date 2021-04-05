a7d38abf11a4ec89283da6d0a3a252907ace7f9d
jeffery <jiang1125712@163.com>
2021-04-06 00:10:08 +0800

    降cpu调整

--- routes/timer.js
+++ routes/timer.js
@@ -6,7 +6,7 @@ const { accountInfo } = AccountConfig
  * 定时任务
  */
 module.exports = (customerWarningCallback) => {
-     /**
+    /**
      * 3秒后开始接收消息队列里的数据
      * */
     setTimeout(() => {
@@ -48,6 +48,7 @@ module.exports = (customerWarningCallback) => {
             if (nextTime < 0) nextTime = 0;
             const hourTimeStr = tempDate.Format("hh:mm:ss")
             const minuteTimeStr = tempDate.Format("mm:ss")
+
             try {
                 if (hourTimeStr == "00:00:00" || hourTimeStr == "08:00:00") {
                     // 每天凌晨零点执行重启服务
@@ -58,56 +59,40 @@ module.exports = (customerWarningCallback) => {
                     Common.restartServer()
                     return
                 }
-                if (hourTimeStr == "00:10:00") {
-                    // 凌晨0点10分重新计算昨天的分析数据
-                    TimerCalculateController.calculateCountByDay(-1)
-                    // Common.calculateCountByDayForTenMinutes(1)
-                } 
+            } catch(e) {
+                log.printError("重启程序出错：", e)
+            }
 
-                if (hourTimeStr == "00:12:00") {
-                    // 凌晨0点12分开始创建第二天的数据库表
-                    console.log("开始创建第二天的数据库表...")
-                    Common.createTable()
-                } 
-
-                if (hourTimeStr == "02:00:00") {
-                    // 凌晨2点开始删除过期的数据库表
-                    Common.startDelete()
+            try {
+                // 如果是凌晨，则计算上一天的分析数据
+                if (hourTimeStr > "00:06:00" && hourTimeStr < "00:12:00") {
+                    TimerCalculateController.calculateCountByDay(minuteTimeStr, -1)
+                } else if (minuteTimeStr > "06:00" && minuteTimeStr < "12:00") {
+                    TimerCalculateController.calculateCountByDay(minuteTimeStr, 0)
                 }
-                if (minuteTimeStr == "00:10") {
-                    // 每小时的第10秒钟，开始执行上一个小时的分析结果
-                    TimerCalculateController.calculateCountByHour(1)
+                // 每小时的前6分钟，会计算小时数据
+                if (minuteTimeStr > "00:00" && minuteTimeStr < "06:00") {
+                    TimerCalculateController.calculateCountByHour(minuteTimeStr, 1, customerWarningCallback)
                 }
-                if (minuteTimeStr == "05:00") {
-                    //  TODO 每小时的第5分钟，开始执行上一个小时的分析结果（防止主从同步延迟）
-                    TimerCalculateController.calculateCountByHour(1, customerWarningCallback)
-
-                    // 每小时的第5分钟，检查一遍所有的统计信息，并发出警告
-                    // Common.checkAnalysisData(customerWarningCallback)
-                }
-                if (minuteTimeStr == "06:00") {
-                    // 每小时的第6分钟，计算一次今天的分析结果
-                    TimerCalculateController.calculateCountByDay(0)
-                }
-                if (minuteTimeStr == "33:00") {
-                    // 每小时的第33分钟，开始修复上一个小时的分析结果，因为读写分离数据有可能会延时
-                    TimerCalculateController.calculateCountByHour(1)
-                }
-                if (minuteTimeStr == "39:00") {
-                    // 每小时的第36分钟，开始修复今天的分析结果，因为读写分离有可能数据会延时
-                    TimerCalculateController.calculateCountByDay(0)
-                }
-                if (minuteTimeStr.substring(1) == "0:00") {
-                    // 每隔10分钟，对一些实时数据进行计算
-                    // Common.calculateCountByDayForTenMinutes(0)
-                }
+                // 每隔1分钟，取出全局变量global.monitorInfo.logCountInMinute的值，并清0
                 if (minuteTimeStr.substring(3) == "00") {
-                    // 每隔1分钟，取出全局变量global.monitorInfo.logCountInMinute的值，并清0
                     global.monitorInfo.logCountInMinuteList.push(global.monitorInfo.logCountInMinute)
                     global.monitorInfo.logCountInMinute = 0
                     if (global.monitorInfo.logCountInMinuteList.length > 60) {
                         global.monitorInfo.logCountInMinuteList.shift()
                     }
+                }
+                // 每小时的51分，开始flush pm2 的日志
+                if (minuteTimeStr == "51:00") {
+                    Common.pm2Flush()
+                }
+                // 凌晨0点52分开始创建第二天的数据库表
+                if (hourTimeStr == "00:52:00") {
+                    Common.createTable()
+                } 
+                // 凌晨2点开始删除过期的数据库表
+                if (hourTimeStr == "02:00:00") {
+                    Common.startDelete()
                 }
             } catch(e) {
                 log.printError("定时器执行报错：", e)
