b11abdf5f2d96f3a9ca31e853fc98f7a4373aa3e
jeffery <jiang1125712@163.com>
2021-06-21 00:02:00 +0800

    修正转码问题

--- util/utils.js
+++ util/utils.js
@@ -127,16 +127,17 @@ const Utils = {
     }
     return tempObj
   },
-  b64EncodeUnicode: function(str) {
+  b64EncodeUnicode: function(tempStr) {
+    const str = encodeURIComponent(tempStr)
     return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
       return String.fromCharCode("0x" + p1)
     }))
   },
   b64DecodeUnicode: function(str) {
     try {
-      return decodeURIComponent(myAtob(str).split('').map(function(c) {
+      return decodeURIComponent(decodeURIComponent(myAtob(str).split('').map(function(c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-      }).join(''));
+      }).join('')));
     } catch (e) {
       return str
     }
