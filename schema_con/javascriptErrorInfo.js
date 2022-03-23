e3450431cc4c95d4c0325e242f14b00950cd1fc9
jeffery <jiang1125712@163.com>
2022-03-23 13:04:22 +0800

    增加simpleErrorMessage的字段长度

--- schema_con/javascriptErrorInfo.js
+++ schema_con/javascriptErrorInfo.js
@@ -70,7 +70,7 @@ const getTableProperty = (DataTypes) => {
     },
     // JS报错的简易信息
     simpleErrorMessage: {
-      type: DataTypes.STRING(200),
+      type: DataTypes.STRING(500),
       allowNull: true,
       field: 'simpleErrorMessage'
     },
