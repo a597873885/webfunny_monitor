181ce098a0bb8ed683a5420f5304e0a8891279a6
yulei <960515179@qq.com>
2022-12-14 23:17:39 +0800

    埋点暴露卡片top查询接口

--- event/routes/routes.js
+++ event/routes/routes.js
@@ -181,6 +181,7 @@ const createRoutes = (router) => {
     router.get('/buryPointCard/detail', BuryPointCardController.detail);
     router.post('/buryPointCard/sort', BuryPointCardController.sort);
     router.post('/buryPointCard/refresh', BuryPointCardController.refresh);
+    router.post('/buryPointCard/groupByQuery', BuryPointCardController.groupByQuery);
     router.get('/buryPointCard/export', BuryPointCardController.export);
     router.get('/buryPointCard/tableDisplay', BuryPointCardController.tableDisplay);
 
