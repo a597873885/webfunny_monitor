4d0c1bebe144d4a1badc27035557d21a489e63c7
yulei <960515179@qq.com>
2023-03-03 12:53:38 +0800

    埋点系统提供查询卡片列表接口

--- event/routes/routes.js
+++ event/routes/routes.js
@@ -178,6 +178,7 @@ const createRoutes = (router) => {
     router.post('/buryPointCard/create', BuryPointCardController.create);
     router.post('/buryPointCard/delete', BuryPointCardController.delete);
     router.post('/buryPointCard/list', BuryPointCardController.getList);
+    router.post('/buryPointCard/getList', BuryPointCardController.getListByPageIdAndName);
     router.post('/buryPointCard/update', BuryPointCardController.update);
     router.get('/buryPointCard/detail', BuryPointCardController.detail);
     router.post('/buryPointCard/sort', BuryPointCardController.sort);
