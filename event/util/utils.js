1c4bb62ba2ce889e940bcdb6b16cd488a60c8c13
yulei <960515179@qq.com>
2023-08-03 21:57:04 +0800

    告警优化:列表加上阈值;刷新接口不触发告警

--- event/util/utils.js
+++ event/util/utils.js
@@ -688,6 +688,12 @@ const Utils = {
     // 走到这里,说明数组或者对象中所有元素都相同,返回true
     return true;
   },
+  //比较list
+  getArrDifference : (arr1, arr2) => {
+    return arr1.concat(arr2).filter((v, i, arr) => {
+      return arr.indexOf(v) === arr.lastIndexOf(v);
+    })
+  },
    /**
    * 通过webfunny系统发送邮件
    */
