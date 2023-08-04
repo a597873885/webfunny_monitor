c8a3fc989fd4c6aa8dc29e0f032ffc8851a374cf
jeffery <jiang1125712@163.com>
2023-08-04 16:05:45 +0800

    增加referrer的长度

--- monitor/schema_con/customerPV.js
+++ monitor/schema_con/customerPV.js
@@ -112,7 +112,7 @@ const getTableProperty = (DataTypes) => {
     },
     // 来源页面的URL
     referrer: {
-      type: DataTypes.STRING(200),
+      type: DataTypes.STRING(500),
         allowNull: true,
         field: 'referrer'
     },
