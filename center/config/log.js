66460dba79be02a1ea04e1c292757f6943590136
jeffery <jiang1125712@163.com>
2023-09-26 01:09:12 +0800

    修复log获取ip报错问题

--- center/config/log.js
+++ center/config/log.js
@@ -50,9 +50,9 @@ var formatReqLog = function(ctx, resTime) {
   var body = ctx.request.body
   let getClientIp = function (req) {
     return  req.headers['x-forwarded-for'] ||
-      req.connection.remoteAddress ||
-      req.socket.remoteAddress ||
-      req.connection.socket.remoteAddress;
+      req?.connection?.remoteAddress ||
+      req?.socket?.remoteAddress ||
+      req?.connection?.socket?.remoteAddress;
   };
   let ip = getClientIp(req);
 
